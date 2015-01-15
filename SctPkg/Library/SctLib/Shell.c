daced6543f60a27ace252ff4cce00989e432d7bf
ericjin <ericjin@users.noreply.github.com>
2015-01-15 09:57:35 +0800

    Fix a bug about searching FS% file system failure in shell1.0.
    Signed-off-by: Jin Eric <eric.jin@intel.com>

--- SctPkg/Library/SctLib/Shell.c
+++ SctPkg/Library/SctLib/Shell.c
@@ -35,11 +35,12 @@
   DOCUMENT, WHETHER OR NOT SUCH PARTY HAD ADVANCE NOTICE OF
   THE POSSIBILITY OF SUCH DAMAGES.
 
-  Copyright 2006 - 2012 Unified EFI, Inc. All
+  Copyright 2006 - 2014 Unified EFI, Inc. All
   Rights Reserved, subject to all existing rights in all
   matters included within this Test Suite, to which United
   EFI, Inc. makes no claim of right.
 
+  Copyright (c) 2014, Intel Corporation. All rights reserved.<BR>
   Copyright (c) 2013-2014, ARM Ltd. All rights reserved.
 
 --*/
@@ -394,13 +395,22 @@ SctShellMapToDevicePath (
     if (Name[NameLength - 1] == L':') {
       InternalName = SctAllocateCopyPool (NameLength * sizeof (CHAR16), Name);
       InternalName[NameLength - 1] = L'\0';
-      Status = mEfiShellEnvironment2->GetFsDevicePath (InternalName, DevicePath);
+      *DevicePath = (EFI_DEVICE_PATH_PROTOCOL *) mEfiShellEnvironment2->GetMap (InternalName);
+      if (*DevicePath == NULL) {
+        Status = EFI_NOT_FOUND;
+      }else {
+        Status = EFI_SUCCESS;
+      }
       SctFreePool (InternalName);
       return Status;
     } else {
-      return mEfiShellEnvironment2->GetFsDevicePath (Name, DevicePath);
+      *DevicePath = (EFI_DEVICE_PATH_PROTOCOL *) mEfiShellEnvironment2->GetMap (Name);
+      if (*DevicePath == NULL) {
+        return EFI_NOT_FOUND;
+      }else {
+        return EFI_SUCCESS;
+      }
     }
-
   }
 }
 
