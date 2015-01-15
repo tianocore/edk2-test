81f166b08851eadb984cbf356ef7dccd2760731f
ericjin <eric.jin@intel.com>
2015-01-15 15:58:52 +0800

    Fix a bug in AdapterInfo test case.
    Signed-off-by: ericjin <eric.jin@intel.com>

--- SctPkg/TestCase/UEFI/IHV/Protocol/AdapterInfo/BlackBoxTest/AdapterInfoBBTestConformance.c
+++ SctPkg/TestCase/UEFI/IHV/Protocol/AdapterInfo/BlackBoxTest/AdapterInfoBBTestConformance.c
@@ -35,12 +35,12 @@
   DOCUMENT, WHETHER OR NOT SUCH PARTY HAD ADVANCE NOTICE OF     
   THE POSSIBILITY OF SUCH DAMAGES.                              
                                                                 
-  Copyright 2006 - 2013 Unified EFI, Inc. All  
+  Copyright 2006 - 2014 Unified EFI, Inc. All  
   Rights Reserved, subject to all existing rights in all        
   matters included within this Test Suite, to which United      
   EFI, Inc. makes no claim of right.                            
                                                                 
-  Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>   
+  Copyright (c) 2014, Intel Corporation. All rights reserved.<BR>   
    
 --*/
 /*++
@@ -573,7 +573,7 @@ BBTestSetInformationConformanceTestCheckpoint2 (
                            InformationBlockSize
                            );
       
-    if ( Status != EFI_INVALID_PARAMETER || Status != EFI_WRITE_PROTECTED) {
+    if ( Status != EFI_INVALID_PARAMETER && Status != EFI_WRITE_PROTECTED) {
         AssertionType = EFI_TEST_ASSERTION_FAILED;
     } else {
         AssertionType = EFI_TEST_ASSERTION_PASSED;
