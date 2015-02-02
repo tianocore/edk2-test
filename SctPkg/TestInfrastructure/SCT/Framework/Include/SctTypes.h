2a45f1cc41005ddb69f4d17b64dd00c4d6cec222
ericjin <eric.jin@intel.com>
2015-02-02 09:55:00 +0800

    Correct the Copyright

--- SctPkg/TestInfrastructure/SCT/Framework/Include/SctTypes.h
+++ SctPkg/TestInfrastructure/SCT/Framework/Include/SctTypes.h
@@ -35,12 +35,12 @@
   DOCUMENT, WHETHER OR NOT SUCH PARTY HAD ADVANCE NOTICE OF     
   THE POSSIBILITY OF SUCH DAMAGES.                              
                                                                 
-  Copyright 2006 - 2014 Unified EFI, Inc. All  
+  Copyright 2006 - 2015 Unified EFI, Inc. All  
   Rights Reserved, subject to all existing rights in all        
   matters included within this Test Suite, to which United      
   EFI, Inc. makes no claim of right.                            
                                                                 
-  Copyright (c) 2010 - 2014, Intel Corporation. All rights reserved.<BR>   
+  Copyright (c) 2010 - 2015, Intel Corporation. All rights reserved.<BR>   
    
 --*/
 /*++
