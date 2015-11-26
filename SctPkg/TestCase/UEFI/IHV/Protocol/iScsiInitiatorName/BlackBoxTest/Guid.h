efa6d1343b0071ec429bf341782148442d84628e
ericjin <eric.jin@intel.com>
2015-11-26 16:41:43 +0800

    correct the guid.* to Guid.c.* in iScsiInitatorName module

--- SctPkg/TestCase/UEFI/IHV/Protocol/iScsiInitiatorName/BlackBoxTest/Guid.h
+++ SctPkg/TestCase/UEFI/IHV/Protocol/iScsiInitiatorName/BlackBoxTest/Guid.h
@@ -35,19 +35,19 @@
   DOCUMENT, WHETHER OR NOT SUCH PARTY HAD ADVANCE NOTICE OF     
   THE POSSIBILITY OF SUCH DAMAGES.                              
                                                                 
-  Copyright 2006, 2007, 2008, 2009, 2010 Unified EFI, Inc. All  
+  Copyright 2006 - 2015 Unified EFI, Inc. All  
   Rights Reserved, subject to all existing rights in all        
   matters included within this Test Suite, to which United      
   EFI, Inc. makes no claim of right.                            
                                                                 
-  Copyright (c) 2010, Intel Corporation. All rights reserved.<BR>   
+  Copyright (c) 2010 - 2015, Intel Corporation. All rights reserved.<BR>   
    
 --*/
 /*++
 
 Module Name:
 
-  guid.c
+  Guid.h
 
 Abstract:
 
@@ -55,21 +55,47 @@ Abstract:
 
  --*/
 
-#include "Efi.h"
-#include "guid.h"
+#ifndef __GUID_H__
+#define __GUID_H__
 
-EFI_GUID gIScsiInitiatorNameBBTestFunctionAssertionGuid001 = EFI_TEST_ISCSIINITIATORNAMEBBTESTFUNCTION_ASSERTION_001_GUID;
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTFUNCTION_ASSERTION_001_GUID \
+  { 0xed92f3eb, 0xdda4, 0x4c65, 0xb3, 0x9f, 0x6c, 0x90, 0xfb, 0x2e, 0x77, 0xf9}
 
-EFI_GUID gIScsiInitiatorNameBBTestFunctionAssertionGuid002 = EFI_TEST_ISCSIINITIATORNAMEBBTESTFUNCTION_ASSERTION_002_GUID;
+extern EFI_GUID gIScsiInitiatorNameBBTestFunctionAssertionGuid001;
 
-EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid001 = EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_001_GUID;
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTFUNCTION_ASSERTION_002_GUID \
+  { 0x56cd69be, 0xcfea, 0x4a43, 0xae, 0x1a, 0x41, 0xe4, 0xde, 0x78, 0x83, 0xc8}
 
-EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid002 = EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_002_GUID;
+extern EFI_GUID gIScsiInitiatorNameBBTestFunctionAssertionGuid002;
 
-EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid003 = EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_003_GUID;
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_001_GUID \
+  { 0x4c893a1c, 0x9c28, 0x4038, 0x9a, 0x34, 0xce, 0xe3, 0x15, 0x70, 0xc4, 0xa6}
 
-EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid004 = EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_004_GUID;
+extern EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid001;
 
-EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid005 = EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_005_GUID;
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_002_GUID \
+  { 0x5f4d6864, 0xe8ed, 0x452e, 0xb2, 0xbc, 0x9a, 0xe, 0x6, 0x61, 0x7e, 0x3a}
 
-EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid006 = EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_006_GUID;
+extern EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid002;
+
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_003_GUID \
+  { 0x2502087d, 0xd853, 0x494e, 0xbd, 0xc5, 0x8b, 0x1a, 0xc1, 0x26, 0xd4, 0x61}
+
+extern EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid003;
+
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_004_GUID \
+  { 0x5bd1c13e, 0x1b9c, 0x432f, 0xb9, 0x33, 0xd9, 0xcf, 0x6f, 0xac, 0xd4, 0x2d}
+
+extern EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid004;
+
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_005_GUID \
+  { 0xacb61cfd, 0xe82b, 0x4250, 0xb0, 0x60, 0xdb, 0x18, 0x55, 0x9e, 0x58, 0xb1}
+
+extern EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid005;
+
+#define EFI_TEST_ISCSIINITIATORNAMEBBTESTCONFORMANCE_ASSERTION_006_GUID \
+  { 0xdc419b8e, 0xb074, 0x4388, 0xbb, 0x85, 0xc8, 0xed, 0xa0, 0x19, 0x95, 0xd3}
+
+extern EFI_GUID gIScsiInitiatorNameBBTestConformanceAssertionGuid006;
+
+#endif __GUID_H__
