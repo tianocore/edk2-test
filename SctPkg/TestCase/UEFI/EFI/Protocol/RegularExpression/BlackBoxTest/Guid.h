ef315419e1c6611649e7b34cf8f746371dd3360b
Anoop TK <anoop.tk@hpe.com>
2016-02-08 22:40:45 +0530

    Adding Regular Expression Protocol test case.

---
+++ SctPkg/TestCase/UEFI/EFI/Protocol/RegularExpression/BlackBoxTest/Guid.h
@@ -0,0 +1,110 @@
+/*++
+  The material contained herein is not a license, either        
+  expressly or impliedly, to any intellectual property owned    
+  or controlled by any of the authors or developers of this     
+  material or to any contribution thereto. The material         
+  contained herein is provided on an "AS IS" basis and, to the  
+  maximum extent permitted by applicable law, this information  
+  is provided AS IS AND WITH ALL FAULTS, and the authors and    
+  developers of this material hereby disclaim all other         
+  warranties and conditions, either express, implied or         
+  statutory, including, but not limited to, any (if any)        
+  implied warranties, duties or conditions of merchantability,  
+  of fitness for a particular purpose, of accuracy or           
+  completeness of responses, of results, of workmanlike         
+  effort, of lack of viruses and of lack of negligence, all     
+  with regard to this material and any contribution thereto.    
+  Designers must not rely on the absence or characteristics of  
+  any features or instructions marked "reserved" or             
+  "undefined." The Unified EFI Forum, Inc. reserves any         
+  features or instructions so marked for future definition and  
+  shall have no responsibility whatsoever for conflicts or      
+  incompatibilities arising from future changes to them. ALSO,  
+  THERE IS NO WARRANTY OR CONDITION OF TITLE, QUIET ENJOYMENT,  
+  QUIET POSSESSION, CORRESPONDENCE TO DESCRIPTION OR            
+  NON-INFRINGEMENT WITH REGARD TO THE TEST SUITE AND ANY        
+  CONTRIBUTION THERETO.                                         
+                                                                
+  IN NO EVENT WILL ANY AUTHOR OR DEVELOPER OF THIS MATERIAL OR  
+  ANY CONTRIBUTION THERETO BE LIABLE TO ANY OTHER PARTY FOR     
+  THE COST OF PROCURING SUBSTITUTE GOODS OR SERVICES, LOST      
+  PROFITS, LOSS OF USE, LOSS OF DATA, OR ANY INCIDENTAL,        
+  CONSEQUENTIAL, DIRECT, INDIRECT, OR SPECIAL DAMAGES WHETHER   
+  UNDER CONTRACT, TORT, WARRANTY, OR OTHERWISE, ARISING IN ANY  
+  WAY OUT OF THIS OR ANY OTHER AGREEMENT RELATING TO THIS       
+  DOCUMENT, WHETHER OR NOT SUCH PARTY HAD ADVANCE NOTICE OF     
+  THE POSSIBILITY OF SUCH DAMAGES.                              
+                                                                
+  Copyright 2006 - 2013 Unified EFI, Inc. All  
+  Rights Reserved, subject to all existing rights in all        
+  matters included within this Test Suite, to which United      
+  EFI, Inc. makes no claim of right.                            
+                                                                
+  Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>   
+   
+--*/
+/*++
+
+Module Name:
+
+  guid.h
+
+Abstract:
+
+  GUIDs auto-generated for EFI test assertion.
+
+--*/
+
+
+#define EFI_REGULAR_EXPRESSION_TEST_FUNCTION_ASSERTION_001_GUID \
+{ 0x5365a661, 0xdb02, 0x46ed, 0xb8, 0x3e, 0xbc, 0x71, 0x6d, 0x6a, 0x8b, 0xb4 }
+
+extern EFI_GUID gRegExFunctionTestAssertionGuid001;
+
+#define EFI_REGULAR_EXPRESSION_TEST_FUNCTION_ASSERTION_002_GUID \
+{ 0x94407424, 0xc17e, 0x4a28, 0xb7, 0x84, 0x3f, 0x84, 0x39, 0xcf, 0x30, 0x96 }
+
+extern EFI_GUID gRegExFunctionTestAssertionGuid002;
+
+#define EFI_REGULAR_EXPRESSION_TEST_FUNCTION_ASSERTION_003_GUID \
+{ 0x3d3be925, 0xfbf3, 0x425c, 0xbd, 0xd, 0x2b, 0x95, 0x2f, 0xf3, 0xbf, 0xe8 }
+
+extern EFI_GUID gRegExFunctionTestAssertionGuid003;
+
+
+
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_001_GUID \
+{ 0x3219e1b1, 0xac3a, 0x4f53, 0x99, 0x11, 0xf3, 0x25, 0x44, 0x5b, 0xa8, 0x26 }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid001;
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_002_GUID \
+{ 0x5a216f4d, 0xb4fe, 0x486d, 0x8e, 0x2e, 0x7b, 0xf9, 0x98, 0x47, 0x62, 0xbd }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid002;
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_003_GUID \
+{ 0x9cec70a0, 0xfb56, 0x4b7f, 0x95, 0x31, 0xeb, 0xd0, 0x61, 0xa2, 0xcf, 0x8f }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid003;
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_004_GUID \
+{ 0xfdceb7d8, 0x5fb7, 0x43c8, 0x8f, 0xa8, 0xec, 0xf, 0x7f, 0x14, 0x34, 0x29 }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid004;
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_005_GUID \
+{ 0x76813d40, 0xd2a7, 0x4912, 0x9e, 0xc4, 0x96, 0x6b, 0x14, 0x15, 0x4b, 0x51 }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid005;
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_006_GUID \
+{ 0xea3de64c, 0xe402, 0x43a7, 0xb4, 0x77, 0x66, 0xcd, 0xf5, 0x13, 0x1e, 0x85 }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid006;
+
+#define EFI_REGULAR_EXPRESSION_CONFORMANCETEST_ASSERTION_007_GUID \
+{ 0x98dee30e, 0xdc2b, 0x4dc6, 0x83, 0x10, 0xf8, 0x85, 0x17, 0x2f, 0x4c, 0xc8 }
+
+extern EFI_GUID gRegExConformanceTestAssertionGuid007;
