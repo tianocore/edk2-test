817939095d41afc9a901b58b42e091b4c897ef1b
Eric Jin <eric.jin@intel.com>
2016-07-18 12:50:24 +0800

    No Function Change Modify the Copyright according to the submission b8ca39eba4f1f3115c16b923415a1de756de6bf3

--- SctPkg/UEFI/Protocol/TimeStamp.h
+++ SctPkg/UEFI/Protocol/TimeStamp.h
@@ -35,12 +35,12 @@
   DOCUMENT, WHETHER OR NOT SUCH PARTY HAD ADVANCE NOTICE OF     
   THE POSSIBILITY OF SUCH DAMAGES.                              
                                                                 
-  Copyright 2006 - 2013 Unified EFI, Inc. All  
+  Copyright 2006 - 2016 Unified EFI, Inc. All  
   Rights Reserved, subject to all existing rights in all        
   matters included within this Test Suite, to which United      
   EFI, Inc. makes no claim of right.                            
                                                                 
-  Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>   
+  Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved.<BR>   
    
 --*/
 /*++         
