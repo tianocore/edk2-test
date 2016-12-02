4ffc60d60272746a138a9322f6ebd615fef41fb1
Eric Jin <eric.jin@intel.com>
2016-12-02 16:35:12 +0800

    Revert "Fix ExitBootService_Conf test case"
    This reverts commit 9070b4c3a40e0cf65ba2b5d58efc66745d02bd3d and b78b86ea1b23067b68be82d1c895618ef366a198

--- SctPkg/TestCase/UEFI/EFI/BootServices/ImageServices/BlackBoxTest/ImageBBTestConformance.c
+++ SctPkg/TestCase/UEFI/EFI/BootServices/ImageServices/BlackBoxTest/ImageBBTestConformance.c
@@ -55,7 +55,6 @@ Abstract:
 
 --*/
 
-#include <Protocol/TestRecoveryLibrary.h>
 #include "SctLib.h"
 #include "Misc.h"
 
@@ -63,14 +62,6 @@ Abstract:
   { 0xF6FAB04F, 0xACAF, 0x4af3, { 0xB9, 0xFA, 0xDC, 0xF9, 0x7F, 0xB4, 0x42, 0x6F } }
 
 #define MAX_BUFFER_SIZE  10
-
-// We will write this value as a reset record before system reset
-#define RESET_RECORD_FLAG  0x1
-
-#define RESET_RECORD_BUFFER_SIZE  1024
-
-// Allocate this as a global to avoid allocation on the stack
-UINT8 ResetRecordBuffer[RESET_RECORD_BUFFER_SIZE];
 
 EFI_GUID gTestVendor1Guid = TEST_VENDOR1_GUID;
 
@@ -819,7 +810,6 @@ BBTestExitBootServicesConsistencyTest (
 {
   EFI_STANDARD_TEST_LIBRARY_PROTOCOL   *StandardLib;
   EFI_STATUS                           Status;
-  EFI_TEST_RECOVERY_LIBRARY_PROTOCOL   *RecoveryLib;
   EFI_TEST_ASSERTION                   AssertionType;
   UINTN                                MapKey;
 
@@ -827,8 +817,6 @@ BBTestExitBootServicesConsistencyTest (
   UINTN                                DataSize;
   UINT8                                Data[MAX_BUFFER_SIZE];
   EFI_STATUS                           ReturnStatus;
-
-  UINTN                                ResetRecordBufferSize;
 
   //
   // Init
@@ -844,18 +832,6 @@ BBTestExitBootServicesConsistencyTest (
                    (VOID **) &StandardLib);
   if (EFI_ERROR(Status)) {
     return Status;
-  }
-
-  //
-  // Locate the test recovery libraries
-  //
-  Status = gtBS->LocateProtocol (
-                   &gEfiTestRecoveryLibraryGuid,
-                   NULL,
-                   (VOID**) &RecoveryLib
-                   );
-  if (EFI_ERROR (Status)) {
-   return Status;
   }
 
   Status = ImageTestCheckForCleanEnvironment (&Numbers);
@@ -874,43 +850,17 @@ BBTestExitBootServicesConsistencyTest (
     return Status;
   }
 
-  //
-  // Look for the recovery record
-  //
-  Status = RecoveryLib->ReadResetRecord (
-                          RecoveryLib,
-                          &ResetRecordBufferSize,
-                          ResetRecordBuffer
-                          );
-  if ((Status != EFI_SUCCESS) && (Status != EFI_NOT_FOUND)) {
-    StandardLib->RecordAssertion (
-                   StandardLib,
-                   EFI_TEST_ASSERTION_FAILED,
-                   gTestGenericFailureGuid,
-                   L"ReadResetRecord failed ",
-                   L"%a:%d:Status - %r",
-                   __FILE__,
-                   (UINTN)__LINE__,
-                   Status
-                   );
-     return Status;
-  }
+  DataSize = MAX_BUFFER_SIZE;
+  Status = gtRT->GetVariable (
+                 L"ExitBootServicesTestVariable",             // VariableName
+                 &gTestVendor1Guid,                           // VendorGuid
+                 NULL,                                        // Attributes
+                 &DataSize,                                   // DataSize
+                 &ReturnStatus                                // Data
+                 );
 
-  if ((ResetRecordBuffer[0] == RESET_RECORD_FLAG)) {
-    // Successful reading of the reset record indicates
-    // normal system reset by this test
-    DataSize = MAX_BUFFER_SIZE;
-    Status = gtRT->GetVariable (
-                     L"ExitBootServicesTestVariable",             // VariableName
-                     &gTestVendor1Guid,                           // VendorGuid
-                     NULL,                                        // Attributes
-                     &DataSize,                                   // DataSize
-                     &ReturnStatus                                // Data
-                    );
-
-    if (Status == EFI_SUCCESS) {
-      goto CheckResult;
-    }
+  if (Status == EFI_SUCCESS) {
+    goto CheckResult;
   }
 
   //
@@ -963,19 +913,6 @@ BBTestExitBootServicesConsistencyTest (
                      &ReturnStatus                              // Data
                      );
 
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  // Write the reset record before initiating
-  // system reset
-  ResetRecordBufferSize = sizeof (UINT8);
-  ResetRecordBuffer[0] = RESET_RECORD_FLAG;
-  Status = RecoveryLib->WriteResetRecord (
-                          RecoveryLib,
-                          ResetRecordBufferSize,
-                          ResetRecordBuffer
-                          );
   //reset system
   gtRT->ResetSystem (EfiResetCold, EFI_SUCCESS, 0, NULL);
 
