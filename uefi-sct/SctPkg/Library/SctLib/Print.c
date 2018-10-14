48da96ab4b411a0e6049deda4c9d6cf3b6148a7a
Eric Jin <eric.jin@intel.com>
2018-10-14 09:56:59 +0800

    uefi-sct/SctPkg:Fix the incorrect buffer free in SctAPrint()
    Cc: Supreeth Venkatesh <supreeth.venkatesh@arm.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Eric Jin <eric.jin@intel.com>
    Reviewed-by: Supreeth Venkatesh <supreeth.venkatesh@arm.com>

--- uefi-sct/SctPkg/Library/SctLib/Print.c
+++ uefi-sct/SctPkg/Library/SctLib/Print.c
@@ -2,7 +2,7 @@
 
   Copyright 2006 - 2015 Unified EFI, Inc.<BR>
   Copyright (c) 2013 - 2014, ARM Ltd. All rights reserved.<BR>
-  Copyright (c) 2014 - 2015, Intel Corporation. All rights reserved.<BR>   
+  Copyright (c) 2014 - 2018, Intel Corporation. All rights reserved.<BR>   
 
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
@@ -676,7 +676,9 @@ _IPrint (
   }
 
   ret = _Print (&ps);
-  SctFreePool(ps.fmt.u.pw);
+  if (fmt) {
+    SctFreePool(ps.fmt.u.pw);
+  }
   return ret;
 }
 
