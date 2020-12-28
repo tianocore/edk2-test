55e73ded23097a5af96161d42b32b0ab46afaa06
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-12-29 04:08:20 +0800

    uefi-sct/SctPkg: typo ExtendedVeirification
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3142
    
    The parameter name ExtendedVerification has repeatedly been misspelled.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Reviewed-By: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>
    Reviewed-By: G Edhaya Chandran <edhaya.chandran@arm.com>

--- uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/SimpleTextIn/BlackBoxTest/SimpleTextInBBTestStress.c
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/SimpleTextIn/BlackBoxTest/SimpleTextInBBTestStress.c
@@ -141,7 +141,7 @@ BBTestReadKeyStrokeManualTest (
                    StandardLib,
                    AssertionType,
                    gTestGenericFailureGuid,
-                   L"EFI_SIMPLE_TEXT_IN_PROTOCOL.Reset - Reset with ExtendedVeirification as TRUE",
+                   L"EFI_SIMPLE_TEXT_IN_PROTOCOL.Reset - Reset with ExtendedVerification as TRUE",
                    L"%a:%d, Status = %r",
                    __FILE__,
                    (UINTN)__LINE__,
