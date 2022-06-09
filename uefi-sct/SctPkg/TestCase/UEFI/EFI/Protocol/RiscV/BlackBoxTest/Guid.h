fa67ac1381d7bbf3c9dcc980aeee2568af8eef80
Sunil V L <sunilvl@ventanamicro.com>
2022-06-09 12:09:14 +0530

    uefi-sct/SctPkg: Add test cases for RISCV_EFI_BOOT_PROTOCOL
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3837
    
    RISC-V platforms need to support new RISCV_EFI_BOOT_PROTOCOL
    to communicate the boot hart ID to the operating system. Add
    the required test cases for this protocol.
    
    The specification of the protocol is maintained at:
    https://github.com/riscv-non-isa/riscv-uefi
    
    The test case specification is attached in the Bugzilla.
    
    Cc: G Edhaya Chandran <Edhaya.Chandran@arm.com>
    Cc: Barton Gao <gaojie@byosoft.com.cn>
    Cc: Carolyn Gjertsen <Carolyn.Gjertsen@amd.com>
    Cc: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>
    Cc: Eric Jin <eric.jin@intel.com>
    Cc: Arvin Chen <arvinx.chen@intel.com>
    Cc: Supreeth Venkatesh <Supreeth.Venkatesh@amd.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Cc: Abner Chang <abner.chang@hpe.com>
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Reviewed-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Tested-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

---
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/RiscV/BlackBoxTest/Guid.h
@@ -0,0 +1,50 @@
+/** @file
+
+  Copyright (c) 2022, Ventana Micro Systems Inc. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+/*++
+
+ Module Name:
+
+   Guid.h
+
+ Abstract:
+
+   GUIDs auto-generated for EFI test assertion.
+
+--*/
+
+
+#define EFI_TEST_RISCV_BOOT_PROTOCOL_BBTESTCONFORMANCE_ASSERTION_001_GUID \
+{ 0x5ccf7e22, 0x5ca1, 0x433c, { 0x97, 0xb4, 0xea, 0x3a, 0xb8, 0x49, 0xf7, 0x03 } }
+
+extern EFI_GUID gGetBootHartIdBBTestConformanceAssertionGuid001;
+
+#define EFI_TEST_RISCV_BOOT_PROTOCOL_BBTESTCONFORMANCE_ASSERTION_002_GUID \
+{ 0xa07c08ba, 0x8581, 0x477a, { 0x85, 0xe2, 0x69, 0x88, 0xa5, 0xb3, 0xd4, 0xec } }
+
+extern EFI_GUID gGetBootHartIdBBTestConformanceAssertionGuid002;
+
+#define EFI_TEST_RISCV_BOOT_PROTOCOL_BBTESTCONFORMANCE_ASSERTION_003_GUID \
+{ 0x00b63b3a, 0xd91d, 0x4175, { 0x8c, 0xee, 0xf9, 0xca, 0x63, 0xf5, 0x84, 0x1c } }
+
+extern EFI_GUID gGetBootHartIdBBTestConformanceAssertionGuid003;
+
+#define EFI_TEST_RISCV_BOOT_PROTOCOL_BBTESTFUNCTION_ASSERTION_001_GUID \
+{ 0xe3a8b4a8, 0x866c, 0x4513, { 0x86, 0xb3, 0x61, 0x1d, 0x02, 0x52, 0x2f, 0x81 } }
+
+extern EFI_GUID gGetBootHartIdBBTestFunctionAssertionGuid001;
+
+#define EFI_TEST_RISCV_BOOT_PROTOCOL_BBTESTFUNCTION_ASSERTION_002_GUID \
+{ 0x8fbbf9d8, 0x0ab2, 0x481d, { 0xb5, 0xe0, 0x5f, 0x29, 0x99, 0xd3, 0xcb, 0x34 } }
+
+extern EFI_GUID gGetBootHartIdBBTestFunctionAssertionGuid002;
