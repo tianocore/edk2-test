09b7d056d43da01b3abe772946879e3ed5b5eebe
Chao Li <lichao@loongson.cn>
2022-12-13 10:41:55 +0800

    uefi-sct/SctPkg: Add LoongArch64 platform support
    Code referenced from Aarch64 and Risc-V. Added three test case for
    LoongArch64.
    
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=4192
    
    Cc: G Edhaya Chandran <Edhaya.Chandran@arm.com>
    Cc: Barton Gao <gaojie@byosoft.com.cn>
    Cc: Carolyn Gjertsen <Carolyn.Gjertsen@amd.com>
    Cc: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>
    Cc: Eric Jin <eric.jin@intel.com>
    Cc: Supreeth Venkatesh <Supreeth.Venkatesh@amd.com>
    Signed-off-by: Chao Li <lichao@loongson.cn>
    Reviewed-by: Barton Gao <gaojie@byosoft.com.cn>
    Reviewed-by: G Edhaya Chandran <edhaya.chandran@arm.com>

---
+++ uefi-sct/SctPkg/SCRT/SCRTDriver/LoongArch64/Debug.c
@@ -0,0 +1,81 @@
+/** @file
+
+  Copyright 2006 - 2012 Unified EFI, Inc.<BR>
+  Copyright (c) 2011 - 2012 ARM Ltd. All rights reserved.<BR>
+  Copyright (c) 2021 Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+  Copyright (c) 2023, Loongson Technology Corporation Limited. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+/*++
+
+Module Name:
+
+  Debug.c
+
+--*/
+
+#include "SCRTDriver.h"
+
+UINTN                 mHandOffPtr        =  0;
+
+
+EFI_STATUS
+ConsumeHandOff (
+  IN  UINTN         HandOffAddr,
+  OUT CONF_INFO     *ConfigData
+  )
+{
+   RUNTIME_HANDOFF   *HandOffPtr;
+   //
+   // First fix the memory address of hand off data.
+   //
+   FixAddress(&HandOffAddr);
+   mHandOffPtr = HandOffAddr;
+   HandOffPtr  = (RUNTIME_HANDOFF*)mHandOffPtr;
+   *ConfigData =  HandOffPtr->ConfigureInfo;
+
+   //
+   // Fix MmioBase Address.
+   //
+   FixAddress(&HandOffPtr->DebuggerInfo.MmioBase);
+
+   return EFI_SUCCESS;
+}
+
+
+EFI_STATUS
+Send2UART (
+  CHAR8                *String
+  )
+{
+  //
+  // TODO: On LoongArch platforms use platform specific functions to
+  // write the data to UART.
+  //
+  return EFI_SUCCESS;
+}
+
+
+
+EFI_STATUS
+DebugWorker (
+  IN CHAR8    *String
+  )
+{
+  EFI_STATUS    Status;
+
+  //
+  // Send text message to registered UART.
+  //
+  Status = Send2UART(String);
+  return Status;
+}
