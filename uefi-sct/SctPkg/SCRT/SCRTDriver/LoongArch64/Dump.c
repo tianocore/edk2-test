006d3b9eca5da3210ed58f5249a1605cb6c81b26
Chao Li <lichao@loongson.cn>
2022-12-13 10:41:55 +0800

    uefi-sct/SctPkg: Add LoongArch64 platform support
    Code referenced from Aarch64 and Risc-V. Added three test case for
    LoongArch64.
    
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=4192
    
    Cc: G Edhaya Chandran <Edhaya.Chandran@arm.com>
    Cc: Barton Gao <gaojie@byosoft.com.cn>
    Cc: Carolyn Gjertsen <Carolyn.Gjertsen@amd.com>
    Cc: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>
    Cc: Eric Jin <eric.jin@intel.com>
    Cc: Supreeth Venkatesh <Supreeth.Venkatesh@amd.com>
    Signed-off-by: Chao Li <lichao@loongson.cn>
    Reviewed-by: Barton Gao <gaojie@byosoft.com.cn>
    Reviewed-by: G Edhaya Chandran <edhaya.chandran@arm.com>

---
+++ uefi-sct/SctPkg/SCRT/SCRTDriver/LoongArch64/Dump.c
@@ -0,0 +1,68 @@
+/** @file
+
+  Copyright 2006 - 2012 Unified EFI, Inc.<BR>
+  Copyright (c) 2011 - 2012 ARM Ltd. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+/*++
+
+Module Name:
+
+  Dump.c
+
+--*/
+
+#include "SCRTDriver.h"
+
+VOID
+DumpRuntimeTable()
+{
+  Printf ("\n================Dump Runtime Table===============\n");
+  Printf ("Header Signature = 0x%x\n", VRT->Hdr.Signature);
+
+  Printf ("\n================GetTime Service==============\n");
+  Printf ("GetTime @ 0x%x\n", VRT->GetTime);
+
+  Printf ("\n================SetTime Service==============\n");
+  Printf ("SetTime @ 0x%x\n", VRT->SetTime);
+
+  Printf ("\n================GetWakeupTime Service==============\n");
+  Printf ("GetWakeupTime @ 0x%x\n", VRT->GetWakeupTime);
+
+  Printf ("\n================SetWakeupTime Service==============\n");
+  Printf ("SetWakeupTime @ 0x%x\n", VRT->SetWakeupTime);
+
+  Printf ("\n================GetVariable Service==============\n");
+  Printf ("GetVariable @ 0x%x\n", VRT->GetVariable);
+
+  Printf ("\n================GetNextVariableName Service==============\n");
+  Printf ("GetNextVariableName @ 0x%x\n", VRT->GetNextVariableName);
+
+  Printf ("\n================SetVariable Service==============\n");
+  Printf ("SetVariable @ 0x%x\n", VRT->SetVariable);
+
+  Printf ("\n================GetNextHighMonotonicCount Service==============\n");
+  Printf ("GetNextHighMonotonicCount @ 0x%x\n", VRT->GetNextHighMonotonicCount);
+
+  Printf ("\n================ResetSystem Service==============\n");
+  Printf ("ResetSystem @ 0x%x\n", VRT->ResetSystem);
+#if 0
+  Printf ("\n================UpdateCapsule Service==============\n");
+  Printf ("UpdateCapsule @ 0x%x\n", VRT->UpdateCapsule);
+
+  Printf ("\n================QueryCapsuleCapabilities Service==============\n");
+  Printf ("QueryCapsuleCapabilities @ 0x%x\n", VRT->QueryCapsuleCapabilities);
+
+  Printf ("\n================QueryVariableInfo Service==============\n");
+  Printf ("QueryVariableInfo @ 0x%x\n", VRT->QueryVariableInfo);
+#endif
+}
---
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/DebugSupport/BlackBoxTest/LoongArch64/PlatformIsa.c
@@ -0,0 +1,29 @@
+/** @file
+
+  Copyright 2006 - 2012 Unified EFI, Inc.<BR>
+  Copyright (c) 2023, Loongson Technology Corporation Limited. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+/*++
+
+Module Name:
+
+  PlatformIsa.c
+
+Abstract:
+
+  Platform related Isa definition.
+
+--*/
+
+#include "DebugSupportBBTestMain.h"
+
+EFI_INSTRUCTION_SET_ARCHITECTURE PlatformIsa = IsaLoongArch64;
