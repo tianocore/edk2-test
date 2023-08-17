2fa0ed5d3afe5d54ef27255eb798510c46bca9f2
Abhi.Singh <Abhi.Singh@arm.com>
2023-08-17 10:21:58 -0500

    uefi-sct/SctPkg: TCG Platform Reset Check Test
    SCT spec: https://bugzilla.tianocore.org/show_bug.cgi?id=4374
    
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=4419
    
    -Implement initial test infrastructure for all test cases
     including updates to: CommonGenFramework.sh, UEFI_SCT.dsc,
     and Category.ini.
    -Add Guid.c and Guid.h files to cover the assertions 4/49
     assertions defined in the SCT spec
    -Add TCGMemoryOverwriteRequestBBTestMain.h alongside used
     define/global variables
    -Add Platform Reset Check Test
     -check if the platform creates the MOR & MORLOCK
      EFI variables as defined in the TCG Spec
     -sets MOR bit 0 and validate if the platform
      clears the MOR bit 0 upon reset.
    
    Cc: G Edhaya Chandran <edhaya.chandran@arm.com>
    Cc: Barton Gao <gaojie@byosoft.com.cn>
    Cc: Carolyn Gjertsen <carolyn.gjertsen@amd.com>
    
    Signed-off-by: Abhi Singh <abhi.singh@arm.com>
    
    Reviewed-by: Stuart Yoder <stuart.yoder@arm.com>
    Reviewed-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-by: G Edhaya Chandran <edhaya.chandran@arm.com>
    Reviewed-by: Sunny Wang <sunny.wang@arm.com>

---
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/RuntimeServices/TCGMemoryOverwriteRequest/BlackBoxTest/Guid.h
@@ -0,0 +1,46 @@
+/** @file
+
+  Copyright 2006 - 2016 Unified EFI, Inc.<BR>
+  Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2023, Arm Inc. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+/*++
+
+Module Name:
+
+  guid.h
+
+Abstract:
+
+  GUIDs auto-generated for EFI test assertion.
+
+--*/
+
+#define EFI_TEST_TCGMORTESTFUNCTION_ASSERTION_001_GUID \
+{ 0xd1c31d5f, 0x944f, 0x434c, {0x88, 0x26, 0xa6, 0xfb, 0x00, 0xd4, 0xbb, 0x97 }}
+
+#define EFI_TEST_TCGMORTESTFUNCTION_ASSERTION_002_GUID \
+{ 0xa6c191b1, 0x9bfb, 0x4231, {0xb9, 0x2d, 0x4e, 0xa2, 0x21, 0xf6, 0x4b, 0x5d }}
+
+#define EFI_TEST_TCGMORTESTFUNCTION_ASSERTION_003_GUID \
+{ 0x34d41a18, 0x8ffe, 0x456b, {0xa4, 0x82, 0x97, 0xb9, 0x9c, 0x50, 0x7d, 0x9e }}
+
+#define EFI_TEST_TCGMORTESTFUNCTION_ASSERTION_004_GUID \
+{ 0x002519c6, 0x859f, 0x4d25, {0xb1, 0x36, 0xb2, 0xef, 0x61, 0xe5, 0xd9, 0x6f }}
+
+extern EFI_GUID gTCGMemoryOverwriteRequestTestFunctionAssertionGuid001;
+
+extern EFI_GUID gTCGMemoryOverwriteRequestTestFunctionAssertionGuid002;
+
+extern EFI_GUID gTCGMemoryOverwriteRequestTestFunctionAssertionGuid003;
+
+extern EFI_GUID gTCGMemoryOverwriteRequestTestFunctionAssertionGuid004;
