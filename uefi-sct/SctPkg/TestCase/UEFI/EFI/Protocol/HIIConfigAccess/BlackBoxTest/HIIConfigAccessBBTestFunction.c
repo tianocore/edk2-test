3927ff7e5368036d27602fe856fa869aa248c4f4
Ann Cheng <ann.cheng@arm.com>
2024-11-14 13:33:19 +0000

    uefi-sct/SctPkg: ExtractConfig allows EFI_ACCESS_DENIED with a warning
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4847
    
    EFI_ACCESS_DENIED (The action violated a system policy)
    is an acceptable status for ExtractConfig in UEFI specification.
    SCT now makrs the test case as a warning instead of failure,
    when EFI_ACCESS_DENIED is returned.
    
    Signed-off-by: Ann Cheng <ann.cheng@arm.com>

--- uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/HIIConfigAccess/BlackBoxTest/HIIConfigAccessBBTestFunction.c
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/HIIConfigAccess/BlackBoxTest/HIIConfigAccessBBTestFunction.c
@@ -328,7 +328,8 @@ BBTestExtractConfigFunctionTestCheckpoint1 (
     } else {
       gtBS->FreePool (Results);
     }
-  } else if (EFI_OUT_OF_RESOURCES == Status) {
+  } else if ( (EFI_OUT_OF_RESOURCES == Status) ||
+    (EFI_ACCESS_DENIED == Status) ) {
     AssertionType = EFI_TEST_ASSERTION_WARNING;  
   }else {
     AssertionType = EFI_TEST_ASSERTION_FAILED;
@@ -385,7 +386,8 @@ BBTestExtractConfigFunctionTestCheckpoint1 (
       }
       gtBS->FreePool (Results);
     }
-  } else if (EFI_OUT_OF_RESOURCES == Status) {
+  } else if ( (EFI_OUT_OF_RESOURCES == Status) ||
+    (EFI_ACCESS_DENIED == Status) ) {
     AssertionType = EFI_TEST_ASSERTION_WARNING;  
   }else {
     AssertionType = EFI_TEST_ASSERTION_FAILED;
