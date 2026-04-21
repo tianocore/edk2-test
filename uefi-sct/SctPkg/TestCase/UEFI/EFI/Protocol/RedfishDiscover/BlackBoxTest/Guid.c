5e3c0befbefa9e7b772d2879bb62808a4b1cd841
Carolyn Gjertsen <Carolyn.Gjertsen@amd.com>
2026-04-21 15:20:04 -0500

    SctPkg/RedfishDiscoverBBTest: Apply revised definitions per Mantis 2172
    Update Redfish Discover black-box tests to align with UEFI 2.8C
    (Mantis 2172) revised protocol definitions.
    
    New conformance checkpoints:
    - AcquireRedfishService() with Flags == EFI_REDFISH_DISCOVER_VALIDATION
      only (VALIDATION is a modifier, not a discovery method; implementation
      masks it out before checking Flags == 0)
    - GetNetworkInterfaceList() with NULL ImageHandle
    - AcquireRedfishService() with NULL ImageHandle
    
    Update spec references in file headers and protocol header to cite
    Mantis 2172.
    
    Ref: https://github.com/tianocore/edk2-test/issues/324
    
    Signed-off-by: Carolyn Gjertsen <Carolyn.Gjertsen@amd.com>

---
+++ uefi-sct/SctPkg/TestCase/UEFI/EFI/Protocol/RedfishDiscover/BlackBoxTest/Guid.c
@@ -0,0 +1,53 @@
+/** @file
+
+  Copyright 2006 - 2026 Unified EFI, Inc.<BR>
+  Copyright (c) 2026, AMD Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+/*++
+
+ Module Name:
+
+   Guid.c
+
+ Abstract:
+
+   GUIDs for EFI Redfish Discover Protocol test assertion.
+
+--*/
+#include "Efi.h"
+#include "Guid.h"
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid001 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_001_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid002 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_002_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid003 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_003_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid004 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_004_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid005 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_005_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid006 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_006_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid007 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_007_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestConformanceAssertionGuid008 = EFI_TEST_REDFISH_DISCOVER_BBTESTCONFORMANCE_ASSERTION_008_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestFunctionAssertionGuid001 = EFI_TEST_REDFISH_DISCOVER_BBTESTFUNCTION_ASSERTION_001_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestFunctionAssertionGuid002 = EFI_TEST_REDFISH_DISCOVER_BBTESTFUNCTION_ASSERTION_002_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestFunctionAssertionGuid003 = EFI_TEST_REDFISH_DISCOVER_BBTESTFUNCTION_ASSERTION_003_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestFunctionAssertionGuid004 = EFI_TEST_REDFISH_DISCOVER_BBTESTFUNCTION_ASSERTION_004_GUID;
+
+EFI_GUID gRedfishDiscoverBBTestFunctionAssertionGuid005 = EFI_TEST_REDFISH_DISCOVER_BBTESTFUNCTION_ASSERTION_005_GUID;
